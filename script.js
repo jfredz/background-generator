c491a9ffce040584bb93812097be699082aea1d0
Cristian <mlcristianv@gmail.com>
2019-01-31 13:28:37 +0200

    added everything

---
+++ script.js
@@ -0,0 +1,15 @@
+var body = document.querySelector("#gradient");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var h3 = document.querySelector("h3");
+
+function changeGradient(){
+	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+	h3.textContent = body.style.background;
+}
+
+color1.addEventListener("input", changeGradient)
+color2.addEventListener("input", changeGradient)
+
+
+
